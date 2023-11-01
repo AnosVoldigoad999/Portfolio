d7c95ece316316ad42104a5f01148e49c831baf3
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-11-01 22:04:25 +0100

    done

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -25,8 +25,7 @@ export default function Projects ({ check, setCheck}){
   <label htmlFor='check'><AiOutlineMenu className='menu' onClick={()=>{setCheck(!check)}} /></label>
 </nav>
 <main className="projectmain">
-    <h2>My Portfolio</h2>
-    <h5>Feast your eyes upon my creations!</h5>
+   <h2>My Portfolio</h2>
     <div className="project">
     <a target="_blank" href="https://managelandingpage-ten.vercel.app/" className="imga"><img className="screenshots" alt="port" src="/PICS/manage.png"  /></a>
         <div className="projectdes">
@@ -40,7 +39,6 @@ export default function Projects ({ check, setCheck}){
         <p><b><a target="_blank" href="https://managelandingpage-ten.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/managelandingpage'><AiFillGithub className='github' />Github</a></b></p>
         </div>
     </div>
-    <br />
     <div className="project">
         <a target="_blank" href="https://e-commerce-page-flame.vercel.app/"><img className="screenshots" alt="ecommerce" src="/PICS/ecommerce.png"   /></a>
         <div className="projectdes">
@@ -54,21 +52,19 @@ export default function Projects ({ check, setCheck}){
         <p><b><a target="_blank" href="https://e-commerce-page-flame.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/E-commercePage'><AiFillGithub className='github' />Github</a></b></p>
         </div>
     </div>
-    <br />
-    <div className="project">
-        <a target="_blank" href="https://uzaymah.vercel.app/" ><img className="screenshots" alt="port" src="/PICS/portfolio.png"  /></a>
+    <div className="project" style={{marginTop:`${window.innerWidth<=800?'0px':'250px'}`}}>
+        <a target="_blank" href="https://todoapp-epic.vercel.app/"><img className="screenshots" alt="calculator" src="/PICS/todo.png"  /></a>
         <div className="projectdes">
-        <h4>My Portfolio Site</h4>
+        <h4>Todo App</h4>
         <p>
-            You're on it....👀
+            A Todo App with changeable themes, filter and drag and drop functionalities. Try it out for yourself!. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
         </p>
         <p>
             Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
         </p>
-        <p><b><a target="_blank" href="https://uzaymah.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/Portfolio'><AiFillGithub className='github' />Github</a></b></p>
+        <p><b><a target="_blank" href="https://todoapp-epic.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/todoappEpic.git'><AiFillGithub className='github' />Github</a></b></p>
         </div>
     </div>
-    <br />
     <div className="project">
         <a target="_blank" href="https://advice-generator-three-alpha.vercel.app/"><img className="screenshots" alt="advice" src="/PICS/advice.png"  /></a>
         <div className="projectdes">
@@ -82,7 +78,19 @@ export default function Projects ({ check, setCheck}){
         <p><b><a target="_blank" href="https://advice-generator-three-alpha.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/advice-generator'><AiFillGithub className='github' />Github</a></b></p>
         </div>
     </div>
-    <br />
+    <div className="project">
+        <a target="_blank" href="https://comments-section-orcin.vercel.app/" ><img className="screenshots" alt="comments" src="/PICS/comments.png"  /></a>
+        <div className="projectdes">
+        <h4>Comments Section Component</h4>
+        <p>
+            An interactive Comments section component with CRUD capabilities.
+        </p>
+        <p>
+            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
+        </p>
+        <p><b><a target="_blank" href="https://comments-section-orcin.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/comments-section.git'><AiFillGithub className='github' />Github</a></b></p>
+        </div>
+    </div>
     <div className="project">
         <a target="_blank" href="https://basic-operations-calculator.vercel.app/"><img className="screenshots" alt="calculator" src="/PICS/calculator.png"  /></a>
         <div className="projectdes">
@@ -94,20 +102,6 @@ export default function Projects ({ check, setCheck}){
             Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
         </p>
         <p><b><a target="_blank" href="https://basic-operations-calculator.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/calculator.git'><AiFillGithub className='github' />Github</a></b></p>
-        </div>
-    </div>
-    <br />
-    <div className="project" style={{marginTop:`${window.innerWidth<=800?'0px':'250px'}`}}>
-        <a target="_blank" href="https://todoapp-epic.vercel.app/"><img className="screenshots" alt="calculator" src="/PICS/todo.png"  /></a>
-        <div className="projectdes">
-        <h4>Todo App</h4>
-        <p>
-            A Todo App with changeable themes, filter and drag and drop functionalities. Try it out for yourself!. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
-        </p>
-        <p>
-            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://todoapp-epic.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/todoappEpic.git'><AiFillGithub className='github' />Github</a></b></p>
         </div>
     </div>
 </main>
