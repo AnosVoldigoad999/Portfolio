d230a3306a92370dbe1e1b7589e3f986b878c1d8
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-11-02 20:03:23 +0100

    done

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import {AiFillGithub, AiOutlineMenu} from 'react-icons/ai'
 import {RxExternalLink} from 'react-icons/rx'
 import { useEffect } from "react"
+import { projects } from "./projs"
 export default function Projects ({ check, setCheck}){
     useEffect(()=>{
         setCheck(false)
@@ -26,84 +27,21 @@ export default function Projects ({ check, setCheck}){
 </nav>
 <main className="projectmain">
    <h2>My Portfolio</h2>
-    <div className="project">
-    <a target="_blank" href="https://managelandingpage-ten.vercel.app/" className="imga"><img className="screenshots" alt="port" src="/PICS/manage.png"  /></a>
-        <div className="projectdes">
-        <h4>Manage Landing page.</h4>
+    {projects.map((project, index)=>{
+        return <div className="project" key={index}>
+          <a target="_blank" href={project.siteLink} className={project.name}><img className="screenshots" alt={project.name} src={project.imageLink}  /></a>
+          <div className="projectdes">
+        <h4>{project.name}</h4>
         <p>
-            A landing page for <b>Manage</b>, a software product that helps teams plan and track their tasks, goals and progress. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
+            {project.des} It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
         </p>
         <p>
             Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
         </p>
-        <p><b><a target="_blank" href="https://managelandingpage-ten.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/managelandingpage'><AiFillGithub className='github' />Github</a></b></p>
+        <p><b><a target="_blank" href={project.siteLink}>Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href={project.githubLink}><AiFillGithub className='github' />Github</a></b></p>
         </div>
-    </div>
-    <div className="project">
-        <a target="_blank" href="https://e-commerce-page-flame.vercel.app/"><img className="screenshots" alt="ecommerce" src="/PICS/ecommerce.png"   /></a>
-        <div className="projectdes">
-        <h4>E-commerce product page</h4>
-        <p>
-            A product page for a potential sneaker company, showcasing the product, offers and providing options for adding and removing items from the cart. It is a front-end development challenge on <b><a target="_blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
-        </p>
-        <p>
-            Built with <a href='https://react.dev/'><b>React</b></a> and <a href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://e-commerce-page-flame.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/E-commercePage'><AiFillGithub className='github' />Github</a></b></p>
         </div>
-    </div>
-    <div className="project" style={{marginTop:`${window.innerWidth<=800?'0px':'250px'}`}}>
-        <a target="_blank" href="https://todoapp-epic.vercel.app/"><img className="screenshots" alt="calculator" src="/PICS/todo.png"  /></a>
-        <div className="projectdes">
-        <h4>Todo App</h4>
-        <p>
-            A Todo App with changeable themes, filter and drag and drop functionalities. Try it out for yourself!. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
-        </p>
-        <p>
-            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://todoapp-epic.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/todoappEpic.git'><AiFillGithub className='github' />Github</a></b></p>
-        </div>
-    </div>
-    <div className="project">
-        <a target="_blank" href="https://advice-generator-three-alpha.vercel.app/"><img className="screenshots" alt="advice" src="/PICS/advice.png"  /></a>
-        <div className="projectdes">
-        <h4>Random advice generator</h4>
-        <p>
-            An advice generator that works with the <b><a target="_blank" href="https://api.adviceslip.com/">Advice Slip API</a></b> to generate random quotes of advice. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
-        </p>
-        <p>
-            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://advice-generator-three-alpha.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/advice-generator'><AiFillGithub className='github' />Github</a></b></p>
-        </div>
-    </div>
-    <div className="project">
-        <a target="_blank" href="https://comments-section-orcin.vercel.app/" ><img className="screenshots" alt="comments" src="/PICS/comments.png"  /></a>
-        <div className="projectdes">
-        <h4>Comments Section Component</h4>
-        <p>
-            An interactive Comments section component with CRUD capabilities.
-        </p>
-        <p>
-            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://comments-section-orcin.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/comments-section.git'><AiFillGithub className='github' />Github</a></b></p>
-        </div>
-    </div>
-    <div className="project">
-        <a target="_blank" href="https://basic-operations-calculator.vercel.app/"><img className="screenshots" alt="calculator" src="/PICS/calculator.png"  /></a>
-        <div className="projectdes">
-        <h4>Calculator with changeable themes</h4>
-        <p>
-            A very cool calculator with changeable themes, try it out for yourself!. It is a front-end development challenge on <b><a target="blank" href="https://frontendmentor.io">Frontend Mentor</a></b>.
-        </p>
-        <p>
-            Built with <a target="_blank" href='https://react.dev/'><b>React</b></a> and <a target="_blank" href='https://en.wikipedia.org/wiki/CSS'><b>CSS</b></a>
-        </p>
-        <p><b><a target="_blank" href="https://basic-operations-calculator.vercel.app/">Live site<RxExternalLink className="exlink" /></a></b>&nbsp; <b><a target="_blank" href='https://github.com/AnosVoldigoad999/calculator.git'><AiFillGithub className='github' />Github</a></b></p>
-        </div>
-    </div>
+    })}
 </main>
     </>
 }
---
+++ src/projs.jsx
@@ -0,0 +1,41 @@
+
+
+export const projects = [
+    {name:"Manage Landing page.",
+    des:<>A landing page for <b>Manage</b>, a software product that helps teams plan and track their tasks, goals and progress.</>,
+    imageLink:"/PICS/manage.png",
+    siteLink:"https://managelandingpage-ten.vercel.app/",
+    githubLink:"https://github.com/AnosVoldigoad999/managelandingpage"
+    }, {
+        name:"E-commerce product page.",
+        des:<>A product page for a potential sneaker company, showcasing the product, offers and providing options for adding and removing items from the cart.</>,
+        imageLink:"/PICS/ecommerce.png",
+        siteLink:"https://e-commerce-page-flame.vercel.app/",
+        githubLink:"https://github.com/AnosVoldigoad999/E-commercePage"   
+    }, {
+        name:"Todo App.",
+        des:<>A Todo App with changeable themes, filter and drag and drop functionalities. Try it out for yourself!.</>,
+        imageLink:"/PICS/todo.png",
+        siteLink:"  https://todoapp-epic.vercel.app/",
+        githubLink:"https://github.com/AnosVoldigoad999/todoappEpic.git"   
+    }, {
+        name:"Random advice generator.",
+        des:<> An advice generator that works with the <b><a target="_blank" href="https://api.adviceslip.com/">Advice Slip API</a></b> to generate random quotes of advice.</>,
+        imageLink:"/PICS/advice.png",
+        siteLink:"https://advice-generator-three-alpha.vercel.app/",
+        githubLink:"https://github.com/AnosVoldigoad999/advice-generator"   
+    }, {
+        name:"Interactive comments section.",
+        des:<>An interactive Comments section component with CRUD capabilities.</>,
+        imageLink:"/PICS/comments.png",
+        siteLink:"https://comments-section-orcin.vercel.app/",
+        githubLink:"https://github.com/AnosVoldigoad999/comments-section.git"   
+    }, {
+        name:"Calculator with changeable themes",
+        des:<> A very cool calculator with changeable themes, try it out for yourself!.</>,
+        imageLink:"/PICS/calculator.png",
+        siteLink:"https://basic-operations-calculator.vercel.app/",
+        githubLink:"https://github.com/AnosVoldigoad999/calculator.git"   
+    }
+  ]
+
